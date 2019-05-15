11f0eafd89e9e0577750fea5f5c239c6ac73a43e
Josh Soref <jsoref@users.noreply.github.com>
2019-05-14 22:23:03 -0400

    IntelliJ/Java: Portable: Confusing argument to varargs method

--- core/src/main/java/hudson/util/spring/BeanBuilder.java
+++ core/src/main/java/hudson/util/spring/BeanBuilder.java
@@ -539,7 +539,7 @@ public class BeanBuilder extends GroovyObjectSupport {
 					}
 					else {
 						currentBeanConfig = springConfig.createSingletonBean(parameterType);
-						callable.call(null);
+						callable.call((Object[]) null);
 					}
 
 					value = currentBeanConfig.getBeanDefinition();
